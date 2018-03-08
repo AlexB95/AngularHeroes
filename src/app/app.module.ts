b4b579b9951d43d12a045aad10544a94f4589c24
Alexander Barriga <45.al3x@gmail.com>
2018-03-07 23:10:35 -0600

    Added Routing & navigation using navbar & added home, heroes, about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,52 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './components/nav/nav.component';
-import { HomeComponent } from './home/home.component';
-
+import { RouterLinkActive } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  /* { path: 'hero/:id', component: HeroDetailComponent },*/
+  {
+    path: 'heroes',
+    component: HeroesComponent,
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    HeroesComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
